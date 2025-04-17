006790b734824aa84d8b04f1976e041dad8cda6d
Alisa Zobova <az@empat.tech>
2025-04-17 18:42:57 +0000

    fix: any telescope path processing added

--- app/dashboard/router.py
+++ app/dashboard/router.py
@@ -5,12 +5,12 @@ from fastapi import APIRouter
 from starlette.requests import Request
 from starlette.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from starlette.staticfiles import StaticFiles
 
 router = APIRouter()
 
 templates = Jinja2Templates(directory= os.path.join(os.path.dirname(os.path.abspath(__file__)), "", "templates"))
 
-@router.get("/", response_class=HTMLResponse)
+# any path processing
+@router.get("/{file_path:path}", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("dashboard.html", {"request": request})
